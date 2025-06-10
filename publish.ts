e7178e30e3151cedb02f90d270f43bf7f1adac7d
Aljoscha Meyer <mail@aljoscha-meyer.de>
2025-06-10 08:49:41 +0200

    Fix publishing script

--- publish.ts
+++ publish.ts
@@ -1,4 +1,4 @@
-import {SimpleFsDeno} from "../simple_fs_deno/mod.ts";
+import { SimpleFsDeno } from "../simple_fs_deno/mod.ts";
 
 const name = "AljoschaMeyer.github.io";
 const pagesBranch = "master";
@@ -7,35 +7,45 @@ const tmpDir = ".tmp_dir_website_publishing_script";
 
 const fs = new SimpleFsDeno("..");
 
-await run("deno", ["task", "build"]);
-fs.copySync(`${name}/build`, tmpDir, "assertive");
-fs.ensureNotSync(`${tmpDir}/.git`);
-await run("git", ["checkout", pagesBranch]);
+try {
+  await run("deno", ["task", "build"]);
+  fs.copySync(`${name}/build`, tmpDir, "assertive");
+  fs.ensureNotSync(`${tmpDir}/.git`);
+  await run("git", ["checkout", pagesBranch]);
 
-for (const file of fs.lsSync(name)) {
+  for (const file of fs.lsSync(name)) {
     if (file !== ".git") {
-        fs.removeSync(`${name}/${file}`);
+      fs.removeSync(`${name}/${file}`);
     }
-}
+  }
 
-for (const file of fs.lsSync(tmpDir)) {
+  for (const file of fs.lsSync(tmpDir)) {
     if (file !== ".git") {
-        fs.moveSync(`${tmpDir}/${file}`, `${name}/${file}`);
+      fs.moveSync(`${tmpDir}/${file}`, `${name}/${file}`);
     }
+  }
+
+  fs.removeSync(tmpDir);
+
+  await run("git", ["add", "-A"]);
+  try {
+    // This throws when there is nothing to commit o.O
+    await run("git", ["commit", "-am", "Publish changes"]);
+  } catch {
+    // do nothing...
+  }
+  await run("git", ["push"]);
+  await run("git", ["checkout", srcBranch]);
+} catch (err) {
+  await run("git", ["checkout", srcBranch]);
+  throw err;
 }
-
-fs.removeSync(tmpDir);
-
-await run("git", ["add", "-A"]);
-await run("git", ["commit", "-am",  "Publish changes"]);
-await run("git", ["push"]);
-await run("git", ["checkout", srcBranch]);
 
 async function run(cmd: string, args: string[]) {
-    const child = new Deno.Command(cmd, {"args": args}).spawn();
-    const status = await child.status;
+  const child = new Deno.Command(cmd, { "args": args }).spawn();
+  const status = await child.status;
 
-    if (!status.success) {
-        throw [cmd, args, status];
-    }
+  if (!status.success) {
+    throw [cmd, args, status];
+  }
 }
